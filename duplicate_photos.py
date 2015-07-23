e9f25ebbc9956036758f17facd531b83597581c1
purplemass <purplemass@gmail.com>
2015-07-23 12:01:10 +0100

    config is external

--- duplicate_photos.py
+++ duplicate_photos.py
@@ -4,23 +4,9 @@ import shutil
 import time
 
 from PIL import Image
-from PIL import ImageFont
 from PIL import ImageDraw
 
-
-config = {
-    'frames': 16,
-    'batches': 500,
-    'start_batch': 1,
-    'source': '/sayfromage/photos_bullet_16_500/',
-    'target': '/sayfromage/photos_bullet_16_5000/',
-    'overlay': True,
-    'overlay-quality': 95,
-    'overwrite': False,
-    'font': ImageFont.truetype("Courier New Bold.ttf", 100),
-    'delay_between_batches': 2,  # in seconds
-    'delay_between_frames': 0.5,  # in seconds
-}
+from config import config
 
 
 def scan_folder():
